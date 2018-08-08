7e3d05a6f8d5ea914c3762acc5976160cece32ab
brad <bradmedeiros@gmail.com>
2018-08-08 13:01:53 -0700

    execute script

--- pluginTypes/common/hooker/src/main.go
+++ pluginTypes/common/hooker/src/main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"net/http"
 	"strconv"
+	"os/exec"
 )
 
 type Options struct {
@@ -45,7 +46,12 @@ func createHttpServer(endpoint string, port uint, onRequest func()) {
 // maybe want rate limiter too, but idk for now
 func getCallExternalScript(scriptLocation string) func() {
 	return func() {
-		fmt.Println("call external placeholder")
+		err:= exec.Command("/bin/sh", "-c", scriptLocation).Run()
+		if err != nil {
+			fmt.Println("error executing script")
+			fmt.Println(err)
+		}
+		
 	}
 }
 
