a51a74641fe2362a2a9f525e2414ba1d6ee11f58
brad medeiros <bradmedeiros0@gmail.com>
2018-09-16 22:33:47 -0700

    added io loop so can push events for debugging

--- src/main.go
+++ src/main.go
@@ -11,11 +11,13 @@ import "./parseConfig"
 import "./mainlogic"
 import "./plugins"
 import "./plugins/pluginResource"
-
+import "./ioLoop"
 
 
 func main() {	
 	// INIT PART
+	fmt.Println("----------INIT SECTION----------\n")
+
 	config, err := parseConfig.ParseYamlConfig("./examples/simple-config.yaml")
 	logic := mainlogic.New(func(nodeIdChange string){
 		fmt.Println("node id change: ", nodeIdChange)
@@ -62,10 +64,32 @@ func main() {
 	// wait for sigterm (aka ctrl-c from terminal)
 	signalChannel := make(chan os.Signal)
 	signal.Notify(signalChannel, os.Interrupt, syscall.SIGTERM)
+
+	fmt.Println("----------SIGNAL WAITING SECTION----------\n")
+
+	listenMessages := func(){
+		commandChannel := make(chan string) 
+		go ioLoop.StartRepl(commandChannel)
+		for true {
+			select {
+				case commandString := <-commandChannel: {
+					if commandString == "quit" {
+						os.Exit(0)
+					}
+				}
+			}
+		}
+	}
+
+	go listenMessages()
 	<-signalChannel
+	
+	//<-signalChannel
+	fmt.Println("----------SIGNAL CAUGHT SECTION----------\n")
 
 
 	// DEINIT
+	fmt.Println("----------DEINIT SECTION----------\n")
 
 	errRemRes := pluginGroup.RemoveResource(config.PluginType, "test-id", options)
 	if errRemRes != nil {
